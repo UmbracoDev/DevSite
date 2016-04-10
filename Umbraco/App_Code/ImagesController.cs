6de848c83f60e3d11b9fc49457df82f8fc6cf065
UmbracoDev <dev@umbraco.org>
2016-04-10 17:50:31 +0200

    Complete VS project

---
+++ Umbraco/App_Code/ImagesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Umbraco.Web.WebApi;
+
+namespace DevSite
+{
+    public class ImagesController : UmbracoApiController
+    {
+        public async Task<HttpResponseMessage> GetJson(string tag)
+        {
+            var url = string.Format("https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags={0}", tag);
+
+            using (var httpClient = new HttpClient())
+            {
+                var input = await httpClient.GetStringAsync(url);
+                HttpContext.Current.Response.Write(input);
+            }
+
+            return new HttpResponseMessage();
+        }
+    }
+}
---
+++ Umbraco/js/main.js
@@ -0,0 +1,31 @@
+﻿var flickrApp = angular.module('flickrApp', []);
+flickrApp.controller('flickrCtrl', function ($scope, $http) {
+    $scope.tag = "dogs";
+    $scope.newest = true;
+    $scope.quantity = 10;
+    $scope.page = 1;
+
+    $scope.search = function () {
+        $http.get('/Umbraco/Api/Images/GetJson?tag=' + $scope.tag).success(function (response) {
+            $scope.page = 1;
+            // cleanup Flickr Json response
+            response = response.replace('jsonFlickrFeed(', '');
+            response = response.replace('})', '}');
+            response = response.replace(/\\'/g, "'");
+
+            response = JSON.parse(response);
+            $scope.images = response.items;
+        });
+    };
+    $scope.search();
+
+    $scope.nextPage = function () {
+        $scope.page++;
+        var from = $scope.quantity * ($scope.page - 1);
+        var to = $scope.quantity * $scope.page;
+        $scope.images = $scope.images.splice(from, to);
+    }
+    $scope.toggle = function (property) {
+        $scope.newest = (property === 'date') ? !$scope.newest : $scope.newest;
+    }
+});
